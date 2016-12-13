5369d0dd3392e6a9fe1d48d507426ddba2f6877c
Margaret Wismer <margaretgwismer.gmail.com>
2016-12-13 17:51:56 -0500

    ruby file not blank

---
+++ spice-main.rb
@@ -0,0 +1,37 @@
+require 'sinatra'
+require 'sendgrid-ruby'
+
+from = SendGrid::Email.new(email: 'test@example.com')
+subject = "Hello there! Please open my email."
+to = SendGrid::Email.new(email: 'margaretgwismer@gmail.com')
+
+content = SendGrid::Content.new(type: 'text/plain', value: 'Hello, Email!')
+
+mail = SendGrid::Mail.new(from, subject, to, content)
+
+sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+
+# send the email
+response = sg.client.mail._('send').post(request_body: mail.to_json)
+# display the response status code and body
+puts response.status_code
+puts response.body	
+get '/' do 
+   erb :home	
+end
+
+get '/about' do
+   erb :about
+end
+
+get '/products' do
+   erb :products
+end
+
+get '/contact' do
+   erb :contact
+end
+
+post '/thankyou' do
+	erb :thankyou
+end
