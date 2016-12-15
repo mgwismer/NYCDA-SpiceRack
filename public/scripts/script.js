d7abbbd4b57f19a8965328e655c0776075215290
Eric Falchier <ericfalchier@gmail.com>
2016-12-15 17:14:22 -0500

    finished dropdown shopping cart

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,9 +1,35 @@
+$document.ready(function () {
+
+
 
 function dropDownMenu () {
+	
+	this.clickCounter = 0
 
 	this.clickDown = function () {
-		document.getElementsByClassName('glyphicon-shopping-cart').style.display = 'block';
+			console.log(this.clickCounter);
+		if (this.clickCounter % 2 === 0) {
+			document.getElementsByClassName('dropdown_shop_cart')[0].style.display = 'block';
+		}
+		else if (this.clickCounter === 1 || this.clickCounter % 2 === 1) {
+			document.getElementsByClassName('dropdown_shop_cart')[0].style.display = 'none';
+		}
+		this.clickCounter = this.clickCounter + 1;
 	}
 }
 
 var mainPage = new dropDownMenu();
+
+
+
+$('.footer')
+
+
+
+})
+
+
+
+/*document.getElementsByClassName('dropdown_shop_cart').addEventListener('click', function() {
+	document.getElementsByClassName('glyphicon glyphicon-shopping-cart').style.display = 'block';
+})*/
