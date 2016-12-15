933888f8a24d132a284e8373217cb084ec69c04b
Eric Falchier <ericfalchier@gmail.com>
2016-12-15 13:24:33 -0500

    glyphicon stylingfix, dropdown incomplete

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,10 +1,9 @@
 
 function dropDownMenu () {
-	
-	this.shoppingCart = document.getElementsByClassName('glyphicon glyphicon-shopping-cart');
-	this.dropDownCart = document.getElementsByClassName('dropdown_shop_cart');
 
-	this.shoppingCart.addEventListener('click', function () {
-		this.dropDownCart.style.display = 'block';
-	})
+	this.clickDown = function () {
+		document.getElementsByClassName('glyphicon-shopping-cart').style.display = 'block';
+	}
 }
+
+var mainPage = new dropDownMenu();
