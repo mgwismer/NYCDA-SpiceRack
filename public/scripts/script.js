319e2c4c1de4acfa4293e0c1fcfd15f9a8ba5a67
Margaret Wismer <margaretgwismer.gmail.com>
2016-12-16 21:15:27 -0500

    updated views, added buttons

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -6,32 +6,15 @@ $(document).ready(function() {
   var spices_seeds = ["cumin","fennel","coriander","mustard","peppercorn","nutmeg"];
   var price_plants = ["1.30","1.50","1.40","1.50","1.30","1.30","1.60"];
   var price_seeds = ["2.30","2.70","2.60","3.20","3.10","4.30"];
-  var images_plants = ["cilantro1.jpg","basil-bsp.jpg","Rosemary.jpg","Sage.jpg",
+  var images_plants = ["cilantro1.jpg","basil-bsp.jpg","thyme.jpg","Rosemary.jpg","Sage.jpg",
      "Oregano2.jpg","tarragon.jpg"];
   var images_seeds = ["cumin1.jpg","fennel1.jpg","coriander1.jpg","mustard1.jpg",
      "peppercorn1.jpg","nutmeg1.jpg"];
   
   var myStore = new Store("SpiceRack");
   myStore.startStore();
-  $('.multi-item-carousel').carousel({
-    interval: false
-  });
-
-// for every slide in carousel, copy the next slide's item in the slide.
-// Do the same for the next, next item.
-  $('.multi-item-carousel .item').each(function(){
-    var next = $(this).next();
-    if (!next.length) {
-      next = $(this).siblings(':first');
-    }
-    next.children(':first-child').clone().appendTo($(this));
-  
-    if (next.next().length>0) {
-      next.next().children(':first-child').clone().appendTo($(this));
-    } else {
-     $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-    }
-  });
+  runCarousel();
+  startEventListeners();
   console.log(myStore.inventory);
   console.log(myStore.inventory.plantSpices);
  
@@ -45,7 +28,8 @@ $(document).ready(function() {
       this.inventory.addToPlantSpices(spices);
       var spices = this.recordSpices(spices_seeds,price_seeds,images_seeds,"seed");
       this.inventory.addToSeedSpices(spices);
-      this.makeProductPage(this.inventory.seedSpices);
+      this.inventory.makeTotalArray();
+      this.makeProductPage(this.inventory.totalSpices);
     }
 
     this.recordSpices = function(spiceType,priceType,imageType,type) {
@@ -59,28 +43,44 @@ $(document).ready(function() {
 
     this.makeProductPage = function(spices) {
       //parentDiv should be the inner Carousel
-      var parentDiv = $("#theCarousel1").children()[0];
+      var parentClass = $(".carousel-inner");
       console.log("in product page");
-      console.log(parentDiv.className);
-      for (var i = 0; i < spices.length; i++) {
-        var divSpice = parentDiv.appendChild(document.createElement("div"));
-        if (i == 0) {
-          divSpice.className = "item active";
-        }
-        else {
-          divSpice.className = "item";
-        }
-        //divSpice.innerText = spices[i].name;
-        var inDivSpice = divSpice.appendChild(document.createElement("div"));
-        inDivSpice.className = "col-md-4";
-        inDivSpice.innerText = spices[i].name;
-        var imgDiv = inDivSpice.appendChild(document.createElement("img"));
-        imgDiv.className = "img-responsive";
-        imgDiv.src = "images/products/"+spices[i].jpgPhoto;
+      console.log(parentClass);
+      console.log(spices);
+      for (var j = 0; j < parentClass.length; j++) {
+        parentDiv = parentClass[j];
+        var spiceCat = spices[j];
+        this.makeCarouselDiv(spiceCat);
       }
-      console.log(parentDiv);
     }
-
+    
+    this.makeCarouselDiv = function(spiceCat) {
+      for (var i = 0; i < spiceCat.length; i++) {
+          var divSpice = parentDiv.appendChild(document.createElement("div"));
+          if (i == 0) {
+           divSpice.className = "item active";
+          }
+          else {
+           divSpice.className = "item";
+          }
+          //divSpice.innerText = spices[i].name;
+          var inDivSpice = divSpice.appendChild(document.createElement("div"));
+          inDivSpice.className = "col-md-4";
+          var imgDiv = inDivSpice.appendChild(document.createElement("img"));
+          imgDiv.className = "img-responsive";
+          imgDiv.src = "images/products/"+spiceCat[i].jpgPhoto;
+          var pDiv = inDivSpice.appendChild(document.createElement("p"));
+          pDiv.innerText = spiceCat[i].name+"  "+spiceCat[i].price;
+          var btnDiv = inDivSpice.appendChild(document.createElement("input"));
+          btnDiv.type = "button";
+          btnDiv.className = "btnDiv";
+          btnDiv.value = "Add to Cart";
+          var textDiv = inDivSpice.appendChild(document.createElement("input"));
+          textDiv.type = "text";
+          textDiv.value = "# of oz.";
+          textDiv.className = "textDiv";
+      }
+    } 
     this.addCustomer = function(customer) {
       this.customers.push(customer);
     }
@@ -108,6 +108,10 @@ $(document).ready(function() {
        this.seedSpices = this.seedSpices.concat(spices);
        this.numSeed += spices.length;
     }
+    this.makeTotalArray = function() {
+      this.totalSpices = [this.plantSpices, this.seedSpices]
+      console.log(this.totalSpices);
+    }
   }
   
   function Customer(idNum) {
@@ -123,5 +127,40 @@ $(document).ready(function() {
        this.spices.push(spice);
        this.numItems += 1;
     }
+  }
+
+  function startEventListeners() {
+    //Using event delegation. The listener is on the parent of the buttons
+    var plantSpices = document.getElementsByClassName("carousel-inner")[0];
+    plantSpices.addEventListener("click", function(e1) {
+      console.log("button clicked");
+      console.log(e1.target);
+    });
+    var seedSpices = document.getElementsByClassName("carousel-inner")[1];
+    seedSpices.addEventListener("click", function(e2) {
+      console.log(e2.target);
+    });
+  }
+  
+  function runCarousel() {
+    $('.multi-item-carousel').carousel({
+     interval: false
+    });
+
+// for every slide in carousel, copy the next slide's item in the slide.
+// Do the same for the next, next item.
+    $('.multi-item-carousel .item').each(function(){
+      var next = $(this).next();
+      if (!next.length) {
+        next = $(this).siblings(':first');
+      }
+      next.children(':first-child').clone().appendTo($(this));
+  
+      if (next.next().length>0) {
+        next.next().children(':first-child').clone().appendTo($(this));
+      } else {
+      $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+      }
+    });
   }
 });
