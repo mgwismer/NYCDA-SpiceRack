005ae6b0a9a204ee51474dad6afdde4606612d19
Sophia Di <backwardsxmarathon@gmail.com>
2016-12-18 20:09:40 -0500

    Adjusted layout on home page and fixed contact page back end in spice-main.

--- spice-main.rb
+++ spice-main.rb
@@ -1,21 +1,6 @@
 require 'sinatra'
 require 'sendgrid-ruby'
 
-from = SendGrid::Email.new(email: 'test@example.com')
-subject = "Hello there! Please open my email."
-to = SendGrid::Email.new(email: 'margaretgwismer@gmail.com')
-
-content = SendGrid::Content.new(type: 'text/plain', value: 'Hello, Email!')
-
-mail = SendGrid::Mail.new(from, subject, to, content)
-
-sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-
-# send the email
-response = sg.client.mail._('send').post(request_body: mail.to_json)
-# display the response status code and body
-puts response.status_code
-puts response.body	
 get '/' do 
    erb :home	
 end
@@ -32,14 +17,29 @@ get '/contact' do
    erb :contact
 end
 
-get '/order' do
-   erb :order
-end
-
-get 'favicon.ico' do
-	"Hello world"
+get '/orders' do
+   erb :orders
 end
 
 post '/thankyou' do
+	puts params
+	@name = params[:name]
+	@email = params[:email]
+	@message = params[:message]
+
+	from = SendGrid::Email.new(email: @email)
+	subject = "#{@name} has reached out to you on your site!"
+	to = SendGrid::Email.new(email: 'backwardsxmarathon@gmail.com')
+	content = SendGrid::Content.new(type: 'text/plain', value: @message)
+
+	mail = SendGrid::Mail.new(from, subject, to, content)
+
+	sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+
+	# send the email
+	response = sg.client.mail._('send').post(request_body: mail.to_json)
+	# display the response status code and body
+	puts response.status_code
+	puts response.body
 	erb :thankyou
 end
